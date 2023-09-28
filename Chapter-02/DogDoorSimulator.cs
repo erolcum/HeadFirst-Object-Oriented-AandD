d488e1e1f54c987bc303afc8846ddcbfe78bdf1f
Erol Iscioglu <110387801+erolcum@users.noreply.github.com>
2023-09-28 17:25:40 +0300

    Add files via upload

---
+++ Chapter-02/DogDoorSimulator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HFOO_02
+{
+    class DogDoorSimulator
+    {
+        static void Main(string[] args)
+        {
+            DogDoor door = new DogDoor();
+            Remote remote = new Remote(door);
+
+            Console.WriteLine("Fido barks to go outside...\n"); 
+
+            remote.pressButton();
+
+            Console.WriteLine("\nFido has gone outside...");           
+            Console.WriteLine("\nFido's all done...");        
+            Console.WriteLine("\nFido's back inside...\n");            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Chapter-02/Remote.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace HFOO_02
+{
+    class Remote
+    {
+        private DogDoor door;
+        private Timer newTimer;
+        public Remote(DogDoor door)
+        {
+            this.door = door;
+            newTimer = new Timer();
+            newTimer.Elapsed += new ElapsedEventHandler(DisplayTimeEvent);
+            newTimer.Interval = 4000;
+        }
+
+        public void pressButton()
+        {
+            Console.WriteLine("Pressing the remote control button...\n");
+            if (door.IsOpen())
+            {
+                door.Close();
+            }
+            else
+            {
+                door.Open();
+                
+                newTimer.Start();
+                
+            }
+        }
+
+        private void DisplayTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            door.Close();
+            newTimer.Stop();
+        }
+    }
+}
