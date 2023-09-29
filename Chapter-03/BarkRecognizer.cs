8ed27ab365109178ade351563d7b00e7f5657a6c
Erol Iscioglu <110387801+erolcum@users.noreply.github.com>
2023-09-29 09:37:44 +0300

    Add files via upload

---
+++ Chapter-03/BarkRecognizer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HFOO_03
+{
+    class BarkRecognizer
+    {
+        private DogDoor door;
+
+        public BarkRecognizer(DogDoor door)
+        {
+            this.door = door;
+        }
+
+        public void recognize(string bark)
+        {
+            Console.WriteLine($"   BarkRecognizer: Heard a '{bark}'");
+            door.Open();
+        }
+    }
+}
---
+++ Chapter-03/DogDoorSimulator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HFOO_03
+{
+    class DogDoorSimulator
+    {
+        static void Main(string[] args)
+        {
+            DogDoor door = new DogDoor();
+            BarkRecognizer recognizer = new BarkRecognizer(door);
+            Remote remote = new Remote(door);
+
+            Console.WriteLine("Fido starts barking.");
+            recognizer.recognize("Woof");
+
+            Console.WriteLine("\nFido has gone outside...");
+
+            Console.WriteLine("\nFido's all done...");
+
+            Thread.Sleep(10000);
+
+            Console.WriteLine("...but he's stuck outside!");
+
+            Console.WriteLine("\nFido starts barking.");
+            recognizer.recognize("Woof");
+
+            Console.WriteLine("\nFido's back inside...");
+
+            Console.ReadLine();
+        }
+    }
+}
