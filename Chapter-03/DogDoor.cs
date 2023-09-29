f46021b1ae2923eb606b97834daf1242dcb46580
Erol Iscioglu <110387801+erolcum@users.noreply.github.com>
2023-09-29 09:41:45 +0300

    Add files via upload

---
+++ Chapter-03/DogDoor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace HFOO_03
+{
+    class DogDoor
+    {
+        private bool open;
+        private Timer newTimer;
+
+        public DogDoor()
+        {
+            this.open = false;
+            newTimer = new Timer();
+            newTimer.Elapsed += new ElapsedEventHandler(DisplayTimeEvent);
+            newTimer.Interval = 4000;
+        }
+
+        private void DisplayTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            Close();
+            newTimer.Stop();
+        }
+
+        public void Open()
+        {
+            Console.WriteLine("The dog door opens."); 
+            open = true;
+            newTimer.Start();
+        }
+
+        public void Close()
+        {
+            Console.WriteLine("The dog door closes.");
+            open = false;
+        }
+
+        public bool IsOpen()
+        {
+            return open;
+        }
+    }
+}
